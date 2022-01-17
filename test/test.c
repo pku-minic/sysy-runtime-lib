49958be4a0f7bbeb1daa379c3d50dd248975565d
MaxXSoft <x@MaxXSoft.net>
2022-01-17 21:15:20 +0800

    (test) updated some details

--- test/test.c
+++ test/test.c
@@ -5,17 +5,22 @@ int main() {
   int arr[20], n, sum = 0;
   n = getarray(arr);
   putarray(n, arr);
-  for (int i = 0; i < n; ++i) sum += arr[i];
-  putint(sum);
+  int count = getint();
+  putint(count);
   putch(10);
-
-  // integer
-  n = getint();
-  putint(-10 + n);
+  starttime();
+  for (int j = 0; j < count; ++j) {
+    for (int k = 0; k < n; ++k) {
+      for (int i = 0; i < n; ++i) sum += arr[i] * arr[k];
+    }
+  }
+  stoptime();
+  putint(sum);
   putch(10);
 
   // read characters
   n = getint();
+  getch();
   for (int i = 0; i < n; ++i) putch(getch());
   putch(10);
 
