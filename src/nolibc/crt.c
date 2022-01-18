f7771a048510fab21fa19a7e894b9ed463adb78b
MaxXSoft <x@MaxXSoft.net>
2022-01-18 11:00:39 +0800

    (crt) added function `memset`

--- src/nolibc/crt.c
+++ src/nolibc/crt.c
@@ -1,8 +1,17 @@
 #include "nolibc/sys.h"
+#include "nolibc/types.h"
 
 extern int main();
 
 void _start() {
   int ret = main();
   SYSCALL1(SYS_EXIT, ret);
+}
+
+void *memset(void *s, int c, size_t len) {
+  unsigned char *p = s;
+  while (len--) {
+    *p++ = (unsigned char)c;
+  }
+  return s;
 }
