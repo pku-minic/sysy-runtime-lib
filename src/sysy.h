631f31eb376bacb6f9a9a496be6de79fa5288c43
MaxXSoft <x@MaxXSoft.net>
2026-03-27 15:59:15 +0800

    (sysy) made the function declarations more readable

--- src/sysy.h
+++ src/sysy.h
@@ -1,15 +1,26 @@
 #ifndef LIBSYSY_SYSY_H_
 #define LIBSYSY_SYSY_H_
 
-// SysY runtime library.
-// Reference: https://bit.ly/3tzTFks
-// Modified by MaxXing.
+// Reads an integer from the standard input.
+int getint();
 
-// Input & output functions
-int getint(), getch(), getarray(int a[]);
-void putint(int num), putch(int ch), putarray(int n, int a[]);
+// Reads a character from the standard input.
+int getch();
 
-// Timing functions
+// Reads an array of integers from the standard input.
+int getarray(int a[]);
+
+// Writes an integer to the standard output.
+void putint(int num);
+
+// Writes a character to the standard output.
+void putch(int ch);
+
+// Writes an array of integers to the standard output.
+void putarray(int n, int a[]);
+
+// Starts and stops the timer.
+// Nested calls to these functions are not allowed.
 void starttime();
 void stoptime();
 
